a97c54043fe7c5cd044b761107f58974c3cab565
Enie Yuliani <enieyuliani.99@gmail.com>
2018-07-03 00:57:47 -0700

    edit controller login

--- application/controllers/logincms/Login.php
+++ application/controllers/logincms/Login.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start(); //we need to start session in order to access it through CI
+// session_start(); //we need to start session in order to access it through CI
 
 Class Login extends CI_Controller
 {
@@ -9,7 +9,7 @@ Class Login extends CI_Controller
 		parent::__construct();
 
 		// Load database
-		$this->load->model('Mlogin');
+		// $this->load->model('Mlogin');
 	}
 
 // Show login page
@@ -49,60 +49,60 @@ Class Login extends CI_Controller
 // 	}
 
 // Check for user login process
-	public function user_login_process() {
+// 	public function user_login_process() {
 
-		$this->form_validation->set_rules('AdmUsr', 'Username', 'trim|required|xss_clean');
-		$this->form_validation->set_rules('AdmPswd', 'Password', 'trim|required|xss_clean');
+// 		$this->form_validation->set_rules('AdmUsr', 'Username', 'trim|required|xss_clean');
+// 		$this->form_validation->set_rules('AdmPswd', 'Password', 'trim|required|xss_clean');
 
-		if ($this->form_validation->run() == FALSE) {
-			if(isset($this->session->userdata['logged_in'])){
-				$this->load->render_page('backend/home');
-			}else{
-				$this->load->view('backend/login');
-			}
-		} 
-		else {
-			$data = array(
-				'username' => $this->input->post('username'),
-				'password' => $this->input->post('password')
-			);
-			$result = $this->login_database->login($data);
-			if ($result == TRUE) {
+// 		if ($this->form_validation->run() == FALSE) {
+// 			if(isset($this->session->userdata['logged_in'])){
+// 				$this->load->render_page('backend/home');
+// 			}else{
+// 				$this->load->view('backend/login');
+// 			}
+// 		} 
+// 		else {
+// 			$data = array(
+// 				'username' => $this->input->post('username'),
+// 				'password' => $this->input->post('password')
+// 			);
+// 			$result = $this->login_database->login($data);
+// 			if ($result == TRUE) {
 
-				$username = $this->input->post('username');
-				$result = $this->login_database->read_user_information($username);
-				if ($result != false) {
-					$session_data = array(
-						'username' => $result[0]->user_name,
-						'email' => $result[0]->user_email,
-					);
-// Add user data in session
-					$this->session->set_userdata('logged_in', $session_data);
-					$this->load->view('admin_page');
-				}
-			} else {
-				$data = array(
-					'error_message' => 'Invalid Username or Password'
-				);
-				$this->load->view('login_form', $data);
-			}
-		}
-	}
+// 				$username = $this->input->post('username');
+// 				$result = $this->login_database->read_user_information($username);
+// 				if ($result != false) {
+// 					$session_data = array(
+// 						'username' => $result[0]->user_name,
+// 						'email' => $result[0]->user_email,
+// 					);
+// // Add user data in session
+// 					$this->session->set_userdata('logged_in', $session_data);
+// 					$this->load->view('admin_page');
+// 				}
+// 			} else {
+// 				$data = array(
+// 					'error_message' => 'Invalid Username or Password'
+// 				);
+// 				$this->load->view('login_form', $data);
+// 			}
+// 		}
+// 	}
 
-// Logout from admin page
-	public function logout() {
+// // Logout from admin page
+// 	public function logout() {
 
-// Removing session data
-		$sess_array = array(
-			'username' => ''
-		);
-		$this->session->unset_userdata('logged_in', $sess_array);
-		$data['message_display'] = 'Successfully Logout';
-		$this->load->view('login_form', $data);
-	}
-
-}
-
+// // Removing session data
+// 		$sess_array = array(
+// 			'username' => ''
+// 		);
+// 		$this->session->unset_userdata('logged_in', $sess_array);
+// 		$data['message_display'] = 'Successfully Logout';
+// 		$this->load->view('login_form', $data);
+// 	}
 
 }
+
+
+
 ?>
