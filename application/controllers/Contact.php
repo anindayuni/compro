fc333e930ccb89b9f10e6294ee86cca6dc629796
anindayuni <anindayuni01@gmail.com>
2018-07-05 09:39:25 +0700

    5 juli

---
+++ application/controllers/Contact.php
@@ -0,0 +1,15 @@
+<?php
+
+class Contact extends MY_Controller
+{
+	
+	// function __construct(argument)
+	// {
+	// 	# code...
+	// }
+	function index()
+	{
+		$this->front_page('frontend/contact');
+	}
+}
+?>
---
+++ application/views/frontend/contact.php
@@ -0,0 +1,68 @@
+<div class="col-sm-8">
+    <article class="single-blog-post">
+        <div class="post-header text-center">
+
+            <h2 class="post-title">Contact us</h2>
+        </div>
+        <div class="post-thumb">
+            <img src="<?= base_url('assets/frontend/images/post-thumb-1.jpg'); ?>" alt="">
+        </div>
+        <div class="post-content">
+            <p>
+                Lorem ipsum dolor sits mate, commenter sad pacing elite, sed diam on my air mod temporal
+                incident laborer et dolor manga aliya at, sed diam voluptuary. At zero Los et accuse eat
+                just to
+                due colors et cerebrum. Stet Clarita and gun egg run, no sea take Mata Sanctus est Lorem
+                ipsum
+                dolor sit meat. Lorem ipsum dolor sits mate, consenteth sad pacing editor, sed diam, not my
+                ear
+            mod temper causal etc just do to Dolores et sebum. Stet Clarita.</p>
+            <p>
+                incident cut laborer et Dolores Magna aliquot eat, sed diam voluptuary. At every es et
+                accused
+                eat just to do colors et ea album. Stet Clarita and gun Berg run, no sea Taki Mata sanctum
+                est
+                Lorem ipsum dolor sit mate.
+            </p>
+        </div>
+        <div id="respond" class="comment-respond">
+            <h3 id="reply-title" class="comment-reply-title">Contact form </h3>
+
+            <form id="contact-form" class="contact-form" name="contact-form" method="post" action="#">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <input type="text" class="form-control" required="required" placeholder="Name"
+                            name="your-name">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <input type="email" class="form-control" required="required"
+                            placeholder="Email Id"
+                            name="your-email">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <input type="text" class="form-control" required="required"
+                            placeholder="Subject"
+                            name="Subject">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <textarea name="Comment" id="message" required="required" class="form-control" rows="7"
+                            placeholder="Message"></textarea>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group text-left">
+                    <button type="submit" class="btn">Send Message</button>
+                </div>
+            </form>
+        </div>
+    </article>
+</div>
