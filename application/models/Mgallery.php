1af15327599784dd31f05fedab29c097121ec111
danis16 <daniswara.daniswara@gmail.com>
2018-07-10 17:26:12 +0700

    galleryyyyyyy

--- application/models/Mgallery.php
+++ application/models/Mgallery.php
@@ -20,7 +20,9 @@ class Mgallery extends CI_Model
 
 
 	function data($number,$offset){
-		return $query = $this->db->query('select * from _article a, _category c, _photo p WHERE a.article_id_category = c.category_id and c.category_type="gallery" and p.photo_id_article=a.article_id order by a.article_id ASC',$number,$offset)->result();		
+		$this->db->limit($number, $offset); 
+		$query=$this->db->query('select * from _article a, _category c, _photo p WHERE a.article_id_category = c.category_id and c.category_type="gallery" and p.photo_id_article=a.article_id order by a.article_id ASC')->result();		
+		return $query;
 	}
 
 
