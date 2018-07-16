e281ad361c5a67bcdc91da1453635d51cffcf2f4
danis16 <daniswara.daniswara@gmail.com>
2018-07-16 13:21:49 +0700

    commit gallery belum jadi

--- application/models/Mgallery.php
+++ application/models/Mgallery.php
@@ -8,32 +8,14 @@ class Mgallery extends CI_Model
 {
 	
 	
-	function getAll($config){  
-		$hasilquery=$this->db->get_where('_photo', $config['per_page'], $this->uri->segment(3));
-		if ($hasilquery->num_rows() > 0) {
-			foreach ($hasilquery->result() as $value) {
-				$data[]=$value;
-			}
-			return $data;
-		}      
-	}
-
-
 	function data($number,$offset){
 		$this->db->limit($number,$offset);
 		$this->db->where('category_type', 'gallery');
 		$this->db->join('_category', '_category.category_id = _article.article_id_category');
 		$this->db->join('_photo', '_photo.photo_id_article = _article.article_id', 'left');
 		$this->db->order_by('article_id', 'DESC');
-		// $ambil = $this->db->get('_article');
 		return $query = $this->db->get('_article')->result();	
 	}
-
-	// function data($number,$offset){
-	// 	$this->db->limit($number, $offset); 
-	// 	$query=$this->db->query('select * from _article a, _category c, _photo p WHERE a.article_id_category = c.category_id and c.category_type="gallery" and p.photo_id_article=a.article_id order by a.article_id ASC')->result();		
-	// 	return $query;
-	// }
 
 
 }
--- application/views/backend/gallery/add.php
+++ application/views/backend/gallery/add.php
@@ -26,75 +26,126 @@
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <div class="card">
                     <div class="header">
-                <h2><strong> Data Gallery </strong></h2>
-                <ul class="header-dropdown">
-                    <li>
-                        <a href="http://localhost/github/company-profile/logincms/gallery"><button class="btn btn-raised btn-primary btn-round waves-effect" type="submit" align="right">Kembali</button></a>
-                    </li>
-                    <li>
-                        &nbsp;
-                    </li>
-                </ul>
-            </div>
+                        <h2><strong> Data Gallery </strong></h2>
+                        <ul class="header-dropdown">
+                            <li>
+                                <a href="http://localhost/github/company-profile/logincms/gallery"><button class="btn btn-raised btn-primary btn-round waves-effect" type="submit" align="right">Kembali</button></a>
+                            </li>
+                            <li>
+                                &nbsp;
+                            </li>
+                        </ul>
+                    </div>
                     <div class="body">
-                        <!-- <form action="" id="frmFileUpload" class="dropzone dz-clickable" method="get" enctype="multipart/form-data"> -->
+                        <!-- <form action="/" id="frmFileUpload" class="dropzone dz-clickable" method="get" enctype="multipart/form-data"> -->
                             <form action="<?php echo base_url('logincms/gallery/add_action') ?>" id="frmFileUpload" class="dropzone dz-clickable" method="get" enctype="multipart/form-data">
                                 <div class="dz-message">
                                     <div class="drag-icon-cph"> <i class="material-icons">touch_app</i> </div>
                                     <h4>Drop files here or click to upload Gallery</h4>
-                                    </div>
+                                </div>
 
-                                    <div class="fallback">
-                                        <input id="file" name="file" type="file" multiple />
-                                    </div>
+                                <div class="fallback">
+                                    <input id="file" name="file" type="file" multiple />
+                                </div>
 
-                                    
-                                </form>
-                            </div>
+
+                            </form>
+
+
+
+
+                            <!-- <form action="<?php echo base_url('logincms/gallery/add_action') ?>" class="dropzone" ></form> -->
+
+
                         </div>
                     </div>
                 </div>
+            </div>
 
 
 
-                <script type="text/javascript">
-                   
-                   $(document).ready(function() {
-                       $(document).on('change'),'#file', function(){
-                            var property=document.getElementById("file").files[0];
-                            var image_name = property.name;
-                            var image_extension = image_name.split('.').pop().toLowerCase();
-                            if(jQuery.inArray(image_extension, ['gif','png','jpg','jpeg']) == -1)
-                            {
-                                alert('invalid image file');
-                            }
-
-                            var image_size = property.size;
-                            if(image_size > 2000000){
-                                alert('gambar kegedean');
-                            }
-                            else {
-                                var form_data = new FormData();
-                                form_data.append("file", property);
-
-                                $.ajax({
-                                    url:"",
-                                    method:"GET",
-                                    data:form_data,
-                                    contentType:false,
-                                    cache:false,
-                                    processData:false,
-                                    success:function(){
-                                            alert('sukses');
-                                    }
-                                })
-                            }
+            <script type="text/javascript">
 
 
-                       }
-                   });
+            // Dropzone class:
+            // var myDropzone = new Dropzone("div#myId", { url: "/file/post"});
+
+            // jQuery
+            $("#frmFileUpload").dropzone({ url: "/file/post" });
 
 
-              </script>
+            // "myAwesomeDropzone" is the camelized version of the HTML element's ID
+            // Dropzone.options.myAwesomeDropzone = {
+              // paramName: "file", // The name that will be used to transfer the file
+              // maxFilesize: 6, // MB
+              // dictDefaultMessage:'hahhahahaaa',
+              // dictFileTooBig:alert('gede tenan !'),
+              // success:function(){
+              //   alert('aaaa');},
+               // if(filesize > 1){
+                        // alert('gambar kegedean');
+                    // }
+            //   accept: function(file, done) {
+            //     if (file.name == "justinbieber.jpg") {
+            //       done("Naha, you don't.");
+            //   }
+            //   else { done(); }
+            // }
+            // };
 
-              
+
+            // Prevent Dropzone from auto discovering this element:
+            // Dropzone.options.myAwesomeDropzone = false;
+            // This is useful when you want to create the
+            // Dropzone programmatically later
+
+            // Disable auto discover for all elements:
+            // Dropzone.autoDiscover = false;
+
+            //  $(document).ready(function() {
+            //      $(document).on('change'),'#file', function(){
+            //         var property=document.getElementById("file").files[0];
+            //         var image_name = property.name;
+            //         var image_extension = image_name.split('.').pop().toLowerCase();
+            //         if(jQuery.inArray(image_extension, ['gif','png','jpg','jpeg']) == -1)
+            //         {
+            //             alert('invalid image file');
+            //         }
+
+            //         var image_size = property.size;
+            //         if(image_size > 2000000){
+            //             alert('gambar kegedean');
+            //         }
+            //         else {
+            //             var form_data = new FormData();
+            //             form_data.append("file", property);
+
+            //             $.ajax({
+            //                 url:"",
+            //                 method:"GET",
+            //                 data:form_data,
+            //                 contentType:false,
+            //                 cache:false,
+            //                 processData:false,
+            //                 success:function(){
+            //                     alert('sukses');
+            //                 }
+            //             })
+            //         }
+
+
+            //     }
+            // });
+
+
+
+//             var myDropzone = new Dropzone(document.body, { 
+
+// alert('hahah');
+//     });
+
+
+
+</script>
+
+
